f1fe4417f98ef814c31d483a539e030cbaef96f3
Rishi Raj <rishiraj927947@gmail.com>
2026-02-16 20:38:12 +0530

    check error

--- JAVASCRIPT/DAY-1-BASICS/script.js
+++ JAVASCRIPT/DAY-1-BASICS/script.js
@@ -1,17 +1,7 @@
-console.log("hello");
-console.log("Rishi");
-
-let name = "Rishi";
 let age = 20;
-let isStudent = true;
+age = 21;
+console.log(age);
 
-console.log(name, age, isStudent);
-
-var arr = [1,2,5,10,4]
-console.log(arr);
-
-
-
-
-
-
+const name = "Rishi";
+name = "Raj"; // kya hoga?
+console.log(name);
