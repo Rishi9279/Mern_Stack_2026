03bbbd0121854d6beda0bfcc3b14291514c0228c
Rishi Raj <rishiraj927947@gmail.com>
2026-03-20 20:36:04 +0530

    start function

---
+++ JAVASCRIPT/Function/functionDeclaration.js
@@ -0,0 +1,36 @@
+function greet(name) {
+  console.log("hello " + name);
+}
+greet("Rishi");
+
+function greet1() {
+  console.log("hello Rishi");
+}
+greet1();
+
+
+
+function sum(a, b) {
+  return a + b;
+}
+let ans = sum(4, 5);
+console.log(ans);
+
+
+
+function double(x) {
+  return x * 2;
+}
+let ans1 = double(5);
+console.log(ans1);
+
+
+
+
+
+function test() {
+  console.log(5);
+}
+
+let x = test();
+console.log(x);
