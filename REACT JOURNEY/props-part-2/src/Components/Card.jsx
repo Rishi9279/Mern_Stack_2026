0151e3de0e63ae6d99deee9328f3ec73f04d717d
Rishi Raj <rishiraj927947@gmail.com>
2026-04-20 13:10:45 +0530

    adding icon using lucide

--- REACT JOURNEY/props-part-2/src/Components/Card.jsx
+++ REACT JOURNEY/props-part-2/src/Components/Card.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import { ArrowUpRight } from "lucide-react";
 
-const Card = ({  name, desc, btn, img }) => {
+const Card = ({ name, desc, btn, img }) => {
   return (
     <div className="card">
       <img src={img} alt={name} />
       <h2>{name}</h2>
       <p>{desc}</p>
-      <button>{btn}</button>
+      <button>
+        {btn} <ArrowUpRight size={15} />
+      </button>
     </div>
   );
 };
